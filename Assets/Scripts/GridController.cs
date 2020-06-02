77dadcf45b2a80c8b88f8a3de6f6eda7798023d8
Jothaka <jhkahle@hotmail.de>
2020-06-02 12:59:56 +0200

    Initial project commit. Basic Hexa-grid. Selection of tiles and highlighting.

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridController : MonoBehaviour
+{
+    [SerializeField]
+    private Grid grid;
+
+    [Header("Tilemap references")]
+    [SerializeField]
+    private Tilemap map;
+    [SerializeField]
+    private Tile[] colorTiles;
+
+    [Header("Highlightmap references")]
+    [SerializeField]
+    private Tilemap highlightMap;
+    [SerializeField]
+    private Tile highlightTile;
+
+    private List<GridTileData> currentTiles = new List<GridTileData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            SelectTiles();
+        }
+        else
+        {
+            if(currentTiles.Count >= 3)
+            {
+                highlightMap.ClearAllTiles();
+                //maybe add small effect on highlight map here?!
+
+
+            }
+        }
+    }
+
+    private void SelectTiles()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int tileCoordinate = grid.WorldToCell(mouseWorldPos);
+
+        Tile selectedTile = map.GetTile<Tile>(tileCoordinate);
+        if (selectedTile != null)
+        {
+            GridTileData selectedTileData = new GridTileData(tileCoordinate, selectedTile);
+
+            if (currentTiles.Count == 0)
+            {
+                currentTiles.Add(selectedTileData);
+
+                highlightMap.SetTile(selectedTileData.GridPosition, highlightTile);
+            }
+            else
+            {
+                var currentTileData = currentTiles[currentTiles.Count - 1];
+                if (!currentTiles.Contains(selectedTileData))
+                {
+                    if (currentTileData.IsTileValidNeighbor(selectedTileData))
+                    {
+                        currentTiles.Add(selectedTileData);
+                        highlightMap.SetTile(selectedTileData.GridPosition, highlightTile);
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator ProcessTilesFalling()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+    }
+}
---
+++ Assets/Scripts/GridTileData.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridTileData
+{
+    public Vector3Int GridPosition;
+    public Tile Tile;
+
+    public GridTileData(Vector3Int positiion, Tile tile)
+    {
+        GridPosition = positiion;
+        Tile = tile;
+    }
+
+    public bool IsTileValidNeighbor(GridTileData other)
+    {
+        bool result = false;
+
+        //are the tiles sharing the same tileasset
+        if (other.Tile == Tile)
+        {
+            //is the other tile not this tile
+            if (other.GridPosition != GridPosition)
+            {
+                //is the other left or right of this tile
+                if (other.GridPosition.y <= GridPosition.y + 1 && other.GridPosition.y >= GridPosition.y - 1)
+                {
+                    //due to the nature of the hexagongrid the coordinates may only change on the y-coordinates depending on the positioning of the origin-tile 
+                    //(on even y-positions the top-left and top-right and on odd y-positions the bottom-left and bottom-right)
+                    if((GridPosition.y % 2 == 0 && other.GridPosition.x > GridPosition.x) ||
+                        (GridPosition.y % 2 != 0 && other.GridPosition.x < GridPosition.x))
+                    {
+                        if(other.GridPosition.y == GridPosition.y)
+                        {
+                            result = true;
+                        }
+                    }
+                    else if (other.GridPosition.x <= GridPosition.x + 1 && other.GridPosition.x >= GridPosition.x - 1)
+                    {
+                        result = true;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
